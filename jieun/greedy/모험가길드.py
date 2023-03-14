47024561135f2ae6ef1632c18f71712ea7ccec1d
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-03-15 00:23:32 +0900

    [Greedy] jieun-0314 수정

--- jieun/greedy/모험가길드.py
+++ jieun/greedy/모험가길드.py
@@ -1,19 +1,23 @@
 import sys
+
+
 def gl():
     return sys.stdin.readline().rstrip()
 
-N = int(gl())
-arr = list(map(int,gl().split()))
 
-# 내림차순 정렬
-arr.sort(reverse=True)
+N = int(gl())
+arr = list(map(int, gl().split()))
+
+arr.sort()
 
 ans = 0
-idx = 0 # 현재 보는 그룹 중 공포도가 가장 큰 모험가의 인덱스
+cnt = 0  # 현재 보는 그룹에 속한 인원
 
-# 공포도가 큰 모험가부터 그룹화한다.
-while idx < N:
-    ans += 1
-    idx += arr[idx] # 그룹화 된 모험가 다음 인덱스
+# 공포도가 작은 모험가부터 그룹화한다.
+for x in arr:
+    cnt += 1
+    if cnt == x:  # x는 현재 보는 그룹 중 가장 큰 공포도
+        ans += 1
+        cnt = 0
 
 print(ans)
