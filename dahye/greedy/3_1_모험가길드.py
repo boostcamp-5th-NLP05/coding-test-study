0c1c8fdfb9564bf5009a44bd9d597a7457e75f93
Da-Hye-JUNG <dahye.stat@gmail.com>
2023-03-15 22:24:06 +0900

    [greedy] dahye-0314

--- dahye/greedy/3_1_모험가길드.py
+++ dahye/greedy/3_1_모험가길드.py
@@ -4,14 +4,15 @@ data = list(map(int, input().split()))
 
 #code
 
-data.sort(reverse = True)
-
-count = 0
-while len(data) != 0:
-    for i in range(data[0]-1) :
-        data.pop()
-    del data[0]
-    count += 1
+data.sort()
+count = 0 # 총 그룹 수
+temp = [] # 임의의 그룹
+# 몇 명의 모험가는 마을에 그대로 남아 있어도 된다는 조건을 고려하여 다시작성
+for i in data:
+    temp.append(i)
+    if len(temp)>=max(temp):
+        temp = []
+        count += 1
 
 print(count)
 
--- dahye/greedy/3_2 곱하기 혹은 더하기.py
+++ dahye/greedy/3_2 곱하기 혹은 더하기.py
@@ -4,9 +4,11 @@ data = input()
 
 result = 0
 for i in data:
-    if i != '0' and result !=0 :
-        result = result*int(i)
+    i = int(i)
+    if i == 0 or result ==0 or i == 1 or result == 1 : 
+        #result가 1인 상황도 +가 더 크다는 것을 고려
+        result += i
     else :
-        result += int(i)
+        result = result * i
 print(result)
 
