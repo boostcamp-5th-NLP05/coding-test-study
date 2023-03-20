042cf810081d31590f22922724356b99ade7b497
JLake310 <0310kjy@gmail.com>
2023-03-20 16:24:26 +0900

    [구현] jaeyeon-0320

--- jaeyeon/구현/게임개발.py
+++ jaeyeon/구현/게임개발.py
@@ -1,4 +1,4 @@
-if __name__=='__main__':
+if __name__ == "__main__":
     N, M = map(int, input().split())
     cur_row, cur_col, cur_dir = map(int, input().split())
 
@@ -11,12 +11,12 @@ if __name__=='__main__':
     # 0 : North, 1 : East, 2 : South, 3 : West
     dr = [-1, 0, 1, 0]
     dc = [0, 1, 0, -1]
-    
+
     answer = 1                                  # 시작점도 방문 횟수에 포함
     while True:
         game_map[cur_row][cur_col] = 2          # 한 번 방문한 지점은 방문 못하기 때문에 바다로 만들어 버린다 => 2로 만든다
         turn_cnt = 0
-        while turn_cnt <= 4:                    # 4번 돌면 뒤로 가야하기 때문에 반복문 사용
+        while turn_cnt < 4:                    # 4번 돌면 뒤로 가야하기 때문에 반복문 사용
             turn_cnt += 1
             cur_dir = (cur_dir + 3) % 4         # 반시계 방향이기 때문에 cur_dir - 1이 아닌 + 3
             next_row = cur_row + dr[cur_dir]
@@ -25,7 +25,7 @@ if __name__=='__main__':
             # 다음으로 갈 지점이 조건에 부합한 지 확인. 부합하지 않으면 다음 회전으로 넘어가기
             if next_row < 0 or next_row > 7 or next_col < 0 or next_col > 7:
                 continue
-            
+
             if game_map[next_row][next_col] != 0:
                 continue
 
@@ -34,7 +34,7 @@ if __name__=='__main__':
             cur_col = next_col
             answer += 1
             break
-        
+
         # 4번 회전했을 때 실행되는 부분
         if turn_cnt == 5:
             next_row = cur_row - dr[cur_dir]
--- jaeyeon/구현/왕실의나이트.py
+++ jaeyeon/구현/왕실의나이트.py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
     position = input()
-    col = ord(position[0]) - ord('a')
+    col = ord(position[0]) - ord("a")
     row = int(position[1]) - 1
-    
+
     cnt = 0
     # 반복문으로 다음 위치가 체스판 내에 있는지 확인
-    for i in [-2,2]:
+    for i in [-2, 2]:
         for j in [-1, 1]:
             # 반복문은 4번만 돌리되, row와 col에 i와 j를 각각 더하며 한 반복문 당 포지션 2개 확인
             next_row = row + i
