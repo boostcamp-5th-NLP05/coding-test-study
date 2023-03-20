5557900d077d3509a8c916c94a1564072608eee3
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-03-20 21:20:32 +0900

    [구현] jieun-0321

--- jieun/구현/게임개발.py
+++ jieun/구현/게임개발.py
@@ -12,13 +12,18 @@ step = [(-1, 0), (0, 1), (1, 0), (-1, 0)]
 # step[0]: 북쪽, step[1]: 동쪽, step[2]: 남쪽, step[3]: 서쪽
 
 
-def can_go(nr, nc):
+def can_go(nr, nc, backward=False):
     """(nr,nc)에 갈 수 있는지 반환"""
     # 맵 밖으로 나가는지 확인
     if nr < 0 or nr >= N or nc < 0 or nc >= M:
         return False
-    # 바다이거나 방문한 칸인지 확인
-    if region[nr][nc] != 0 or visited[nr][nc] != False:
+    if backward:  # 뒤로 갈 때 육지이면 방문한 칸이라도 갈 수 있다.
+        if region[nr][nc] == 0:
+            return True
+        else:
+            return False
+    elif region[nr][nc] != 0 or visited[nr][nc] != False:
+        # 바다이거나 방문한 칸인지 확인
         return False
     return True
 
@@ -36,7 +41,7 @@ while True:
         # 방문할 칸 (nr, nc)
         nr = A + step[direction][0]
         nc = B + step[direction][1]
-        if can_go(nr, nc):
+        if can_go(nr, nc, backward=False):
             back = False  # 뒤쪽 방향(매뉴얼 3) 볼 필요 없다.
             A = nr
             B = nc
@@ -46,15 +51,15 @@ while True:
 
     # 네 방향 못 가서 뒤로 가기 시도
     if back:
-        # 뒤쪽 방향: 0 <-> 2, 1 <-> 3
-        direction = (direction + 2) % 4
-        nr = A + step[direction][0]
-        nc = B + step[direction][1]
-        if can_go(nr, nc):
+        # 바라보는 방향 유지하면서 뒤로 이동
+        nr = A - step[direction][0]
+        nc = B - step[direction][1]
+        if can_go(nr, nc, backward=True):
             A = nr
             B = nc
-            visited[nr][nc] = True
-            ans += 1
+            if visited[nr][nc] == False:
+                visited[nr][nc] = True
+                ans += 1
         else:
             break  # 뒤로도 못 가서 루프 탈출
 
