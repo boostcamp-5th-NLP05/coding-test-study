9d4af9ee5198a3a79de5090f489fcc661a74f7d4
JLake310 <0310kjy@gmail.com>
2023-03-27 02:18:01 +0900

    Update 미로 탈출.py

--- jaeyeon/DFSBFS/미로 탈출.py
+++ jaeyeon/DFSBFS/미로 탈출.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
 
     # 현재 지점과 depth를 함께 저장, 여기서의 depth는 한 지점까지의 최소 거리를 뜻함
     queue = deque([[0, 0, 1]])
-
-    while len(queue) != 0:
+    answer = 0
+    while answer == 0:
         cur_r, cur_c, cur_depth = queue.popleft()
         if map_[cur_r][cur_c] == 0:
             continue
@@ -35,9 +35,10 @@ if __name__ == "__main__":
 
             # 다음 방문 지점이 출구라면 그 전 지점의 값 + 1 을 반환 => 최소거리라는 뜻
             if next_r == N - 1 and next_c == M - 1:
-                print(map_[next_r - r][next_c - c] + 1)
+                answer = map_[next_r - r][next_c - c] + 1
                 break
 
             queue.append([next_r, next_c, cur_depth + 1])
 
+print(answer)
 # 27분 20초
