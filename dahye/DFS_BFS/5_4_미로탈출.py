781fbd3890f9f74a23ae4e71a293788da191592d
Da-Hye-JUNG <dahye.stat@gmail.com>
2023-03-28 03:43:05 +0900

    Update 5_4_미로탈출.py
    답이 자꾸 틀려서 모범답안이랑 거의 똑같게 코드를 수정했는데 2가 출력되네요. 추후 고민해봐야 할 것 같습니다.

--- dahye/DFS_BFS/5_4_미로탈출.py
+++ dahye/DFS_BFS/5_4_미로탈출.py
@@ -4,25 +4,23 @@ data = []
 for _ in range(n):
     data.append(list(map(int, input())))
 
-result = 0
 def bfs(x,y) :
-    queue = deque([0,0])
+    queue = deque()
+    queue.append((0,0))
     while queue:
         i, j = queue.popleft()
-        for di, dj in [-1, 0], [1, 0], [0, -1], [0, 1]: #다음 step 확인
+        for di, dj in ([-1, 0], [1, 0], [0, -1], [0, 1]): #다음 step 확인
             ni, nj = i + di, y + dj
 
             if ni>=n or ni<0 or nj>=m or nj<0: #범위넘어가면 다른 경우로
-                return ''
+                continue
             if data[ni][nj] == 0: #괴물이 있으면 다른 경우로
-                return ''
-            if data[ni][nj] == 1: #지름길이면 원래 거리count에 1더해줌
-                queue.append([ni,nj]) 
-                data[ni][nj] = data[i][j] + 1
-        return queue[-1]
-
+                continue
             
-        
+            if data[ni][nj] == 1: #지름길이면 원래 거리count에 1더해줌
+                queue.append((ni,nj)) 
+                data[ni][nj] = data[i][j] + 1
+            
+    return data[n-1][m-1] + 1
 
-        
 print(bfs(0,0))
