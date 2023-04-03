a487875df1d930d284f96b9fbde30d66300a8cc3
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-03 11:34:35 +0900

    fix 두 배열의 원소 교체

--- yunjin/정렬/두 배열의 원소 교체.py
+++ yunjin/정렬/두 배열의 원소 교체.py
@@ -11,7 +11,13 @@ answer = sum(array_A)
 for i in range(K):
 
     # A는 왼쪽에서 B는 오른쪽에서 접근하면서 원소 바꾸면서 더해줌.
-    answer -= array_A[i]
-    answer += array_B[N - 1 - i]
+    left = array_A[i]
+    right = array_B[N - 1 - i]
+
+    if left < right:
+        answer -= left
+        answer += right
+    else:
+        break
 
 print(answer)
