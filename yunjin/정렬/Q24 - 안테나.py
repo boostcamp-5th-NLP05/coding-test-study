5eb21f81d5b9ace6dd489d6fc406f5518c90605d
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-05 11:10:42 +0900

    fix 안테나

--- yunjin/정렬/Q24 - 안테나.py
+++ yunjin/정렬/Q24 - 안테나.py
@@ -10,8 +10,8 @@ left_number_idx = 0
 r = []
 
 for position in postions:
-    answer -= (N - left_number_idx)
-    answer += left_number_idx
+    answer -= (N - left_number_idx) * position # 오른쪽으로 갈 때마다 감소하는 거리
+    answer += left_number_idx * position # 오른쪽으로 갈 때마다 증가하는 거리
     left_number_idx += 1  # 왼쪽 갯수 증가
     r.append((answer, position))
 
