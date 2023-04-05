93425ed1465ed958e06056e1002a4ffabb670883
darae <dahye.stat@gmail.com>
2023-04-05 16:32:10 +0900

    Update 6_Q23_국영수.py

--- dahye/정렬/6_Q23_국영수.py
+++ dahye/정렬/6_Q23_국영수.py
@@ -3,10 +3,8 @@ data = []
 for i in range(N):
     temp = list(input().split())
     data.append([temp[0]] + list(map(int,temp[1:])))
-data.sort(key = lambda x : x[1], reverse=True)
-data.sort(key = lambda x : x[2])
-data.sort(key = lambda x : x[3], reverse=True)
-data.sort(key = lambda x : x[0])
+
+data.sort(key = lambda x : (-x[1],x[2],-x[3],x[0]))
 
 for name in data:
     print(name[0])
