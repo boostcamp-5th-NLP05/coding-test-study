c6cfc671e65bfc6821d9597d6d8304c6403a243e
darae <dahye.stat@gmail.com>
2023-04-06 21:06:06 +0900

    Update 6_Q26_카드정렬하기.py

--- dahye/정렬/6_Q26_카드정렬하기.py
+++ dahye/정렬/6_Q26_카드정렬하기.py
@@ -1,14 +1,26 @@
+import heapq
+
 N = int(input())
 data = []
+
 for i in range(N):
     data.append(int(input()))
-data.sort() #작은 수부터 묶어야함
-sum_list = [] # j번째 합치기에서 비교 수 저장 list
-s = data[0] # j번째 합치기에서 비교 수
-for j in range(1, N):
-    s += data[j]
-    sum_list.append(s)
-print(sum(sum_list)) #총 합치기 수 
+data.sort() 
+heapq.heapify(data) #heap자료형으로 바꿈
+
+answer = 0
+while len(data) != 1:
+    a = heapq.heappop(data)             #최솟값 두개 뽑기
+    b = heapq.heappop(data)             
+
+    answer += a
+    answer += b
+
+    heapq.heappush(data,a+b)            #합친 묶음 다시 넣어주기
+
+print(answer)
+
+
 
 
 
