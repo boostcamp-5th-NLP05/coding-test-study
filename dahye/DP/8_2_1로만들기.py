7c7dba110fbc963c47b3faa222462f81db5fd708
Da-Hye-JUNG <96599427+Da-Hye-JUNG@users.noreply.github.com>
2023-04-11 02:59:01 +0900

    Revert "Update 8_2_1로만들기.py"

--- dahye/DP/8_2_1로만들기.py
+++ dahye/DP/8_2_1로만들기.py
@@ -3,12 +3,13 @@ x = int(input())
 d = [0] * (x+1) #d에 들어갈 것 : x부터 시작해서 각 숫자에 도달할때까지 연산 횟수
 
 for i in range(2, x+1):
-    d[i] = d[i-1]+1 #먼저 1을 더한 값을 넣어주고 비교
     if i % 5 == 0:
-        d[i] = min(d[i],d[i//5]+1)
+        d[i] = min(d[i-1]+1,d[i//5]+1)
     if i % 3 == 0:
-        d[i] = min(d[i],d[i//3]+1)
+        d[i] = min(d[i-1]+1,d[i//3]+1)
     if i % 2 == 0:
-        d[i] = min(d[i],d[i//2]+1)
+        d[i] = min(d[i-1]+1,d[i//2]+1)
+    if i % 5 != 0 and i % 3 != 0 and i % 2 != 0:
+        d[i] = d[i-1]+1
      
 print(d[x])
