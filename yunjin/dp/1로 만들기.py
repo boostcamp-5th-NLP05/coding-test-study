5fb4758bca66d4cb552ac1a0c4a87172aa9034c9
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-04-10 09:59:41 +0900

    fix 1로 만들기

--- yunjin/dp/1로 만들기.py
+++ yunjin/dp/1로 만들기.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 N = int(input())
 
-dp = [0 for i in range(N+1)]
+dp = [0 for i in range(1000001)]
 
 dp[0] = 0
 dp[1] = 0
@@ -12,14 +12,23 @@ dp[3] = 1
 dp[4] = 2
 dp[5] = 1
 
-for i in range(6, N+1):
-    if i % 5 == 0:
+for i in range(3, N+1):
+
+    if i % 5 == 0 and i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i-1], dp[i//5], dp[i//3], dp[i//2]) + 1
+    elif i % 5 == 0 and i % 3 == 0 :
+        dp[i] = min(dp[i-1], dp[i//5], dp[i//3]) + 1
+    elif i % 5 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i-1], dp[i//5], dp[i//2]) + 1
+    elif i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i-1], dp[i//3], dp[i//2]) + 1
+    elif i % 5 == 0:
         dp[i] = min(dp[i-1], dp[i//5]) + 1
     elif i % 3 == 0:
-        dp[i] = min(dp[i-1], dp[i//3]) + 1
+        dp[i] = min(dp[i-1], dp[i//3])+1
     elif i % 2 == 0:
-        dp[i] = min(dp[i-1], dp[i//2]) + 1
+        dp[i] = min(dp[i-1], dp[i//2])+1
     else:
-        dp[i] = dp[i-1] + 1
+        dp[i] = dp[i - 1] + 1
 
 print(dp[N])
