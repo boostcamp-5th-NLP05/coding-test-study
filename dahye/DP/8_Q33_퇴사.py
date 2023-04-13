970fafababc3e890f42e5e0fd6074f16acf93902
darae <dahye.stat@gmail.com>
2023-04-13 14:14:54 +0900

    Update 8_Q33_퇴사.py
    temp로 초기부분과 퇴사전까지 상담할 수 없을때를 max로 업데이트하는 부분을 추가하였습니다. 더 고려할 테스트케이스가 있는것같은데 다른분들 풀이 참고해보면서 파악해보겠습니다!

--- dahye/DP/8_Q33_퇴사.py
+++ dahye/DP/8_Q33_퇴사.py
@@ -6,15 +6,17 @@ for _ in range(N):
     money.append(b)
     term.append(a)
 
-d = money.copy() #d는 i번째 날까지 받을 수 있는 금액의 최대 값
-
+d = [0]*N #d는 i번째 날까지 받을 수 있는 금액의 최대 값
+temp = 0
 for i in range(N):
     for j in range(i):
         before = i-j-term[j] #term만큼의 이전의 날에서 더해줌
         if before >=0 and before <N:
-            d[i] = max(d[j]+money[i], d[i])
-        if N-i < term[i]: #상담을 퇴사전까지 할 수 없을 때
-            d[i] = d[i-1]
+            d[i] = max(d[j]+money[j], d[i])
+            temp = d[i]
+        else: #상담을 퇴사전까지 할 수 없을 때
+            d[i] = temp
+print(d)
 if N == 1 and term[0] > 1:
     print(0)
 else:   
