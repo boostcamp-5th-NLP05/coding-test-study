d8c44e2f3b4a963c2cfa7bdef8c3fcfd8f8e00c1
darae <dahye.stat@gmail.com>
2023-04-13 17:54:35 +0900

    Update 8_Q33_퇴사.py

--- dahye/DP/8_Q33_퇴사.py
+++ dahye/DP/8_Q33_퇴사.py
@@ -6,19 +6,15 @@ for _ in range(N):
     money.append(b)
     term.append(a)
 
-d = [0]*N #d는 i번째 날까지 받을 수 있는 금액의 최대 값
-temp = 0
-for i in range(N):
+d = [0]*(N+1) #d는 i번째 날까지 받을 수 있는 금액의 최대 값
+for i in range(N+1):
     for j in range(i):
         before = i-j-term[j] #term만큼의 이전의 날에서 더해줌
         if before >=0 and before <N:
             d[i] = max(d[j]+money[j], d[i])
-            temp = d[i]
         else: #상담을 퇴사전까지 할 수 없을 때
-            d[i] = temp
-print(d)
+            d[i] = max(d)
 if N == 1 and term[0] > 1:
     print(0)
 else:   
     print(max(d))
-#90퍼쯤에 오답처리....
