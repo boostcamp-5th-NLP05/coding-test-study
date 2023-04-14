1921efc1c525c46fb7acebc85287754f216734db
박영준_T5087 <asvc1207@naver.com>
2023-04-15 03:36:02 +0900

    Update 못생긴수.py

--- youngjun/DP/못생긴수.py
+++ youngjun/DP/못생긴수.py
@@ -1,14 +1,23 @@
 n = int(input())
 cnt = 1
-num = 2
-while True:
-    if num % 2 == 0 or num % 3 == 0 or num % 5 == 0:
-        cnt += 1
-    else:
-        num += 1
-        continue
-        
-    if cnt == n:
-        print(num)
-        break
-    num += 1
+d = [1]
+i_2,i_3,i_5 = 0,0,0
+
+while cnt != n:
+    mul_2 = 2*d[i_2]
+    mul_3 = 3*d[i_3]
+    mul_5 = 5*d[i_5]
+    
+    min_num = min(mul_2,mul_3,mul_5)
+    
+    d.append(min_num)
+    cnt+=1
+    
+    if mul_2 == min_num:
+        i_2 += 1
+    if mul_3 == min_num:
+        i_3 += 1
+    if mul_5 == min_num:
+        i_5 += 1
+
+print(d[-1])
