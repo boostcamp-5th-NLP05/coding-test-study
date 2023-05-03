d1da5cc4d0e0263af27dcb7823158fdbee8bcd7a
JLake310 <0310kjy@gmail.com>
2023-05-03 23:48:30 +0900

    Update Q44-행성 터널.py

--- jaeyeon/그래프/Q44-행성 터널.py
+++ jaeyeon/그래프/Q44-행성 터널.py
@@ -25,26 +25,29 @@ def union(parent, a, b):
 if __name__ == "__main__":
     N = int(input())
 
-    positions = []
-    costs = []
+    # 각 좌표값과 인덱스를 받을 리스트 선언
     x_list = []
     y_list = []
     z_list = []
 
     parent = [i for i in range(N)]
 
+    # 입력 받을 때 각 좌표값과 인덱스를 tuple로 함께 저장
     for idx in range(N):
         x, y, z = map(int, input().split())
         x_list.append((x, idx))
         y_list.append((y, idx))
         z_list.append((z, idx))
 
+    # 좌표를 기준으로 정렬
     x_list.sort()
     y_list.sort()
     z_list.sort()
 
+    # 크루스칼 알고리즘을 위한 경로를 저장할 배열 선언
     tunnels = []
     for idx in range(N - 1):
+        # 나중에 정렬할 필요 없도록 heap으로 저장
         heappush(
             tunnels,
             (x_list[idx + 1][0] - x_list[idx][0], x_list[idx + 1][1], x_list[idx][1]),
