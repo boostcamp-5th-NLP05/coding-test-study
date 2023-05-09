f987525e0723e9fff414846daf5692b4118972d8
박영준_T5087 <asvc1207@naver.com>
2023-05-09 15:09:12 +0900

    Update 청소년 상어.py

--- youngjun/기출/청소년 상어.py
+++ youngjun/기출/청소년 상어.py
@@ -1,12 +1,17 @@
-#map_ = [[],[],[],[]]
-#for i in range(4):
-#    tmp = list(map(int,input().split()))
-#    for j in range(4):
-#        map_[i].append((tmp[j*2],tmp[j*2 + 1]))
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+map_ = [[],[],[],[]]
+for i in range(4):
+    tmp = list(map(int,input().split()))
+    for j in range(4):
+        map_[i].append((tmp[j*2],tmp[j*2 + 1]))
 #map_ = [[(7, 6), (2, 3), (15, 6), (9, 8)], [(3, 1), (1, 8), (14, 7), (10, 1)], [(6, 1), (13, 6), (4, 3), (11, 4)], [(16, 1), (8, 7), (5, 2), (12, 2)]]
 #map_ = [[(16, 7), (1, 4), (4, 3), (12, 8)], [(14, 7), (7, 6), (3, 4), (10, 2)], [(5, 2), (15, 2), (8, 3), (6, 4)], [(11, 8), (2, 4), (13, 5), (9, 4)]]
-map_ = [[(12, 6), (14, 5), (4, 5), (6, 7)], [(15, 1), (11, 7), (3, 7), (7, 5)], [(10, 3), (8, 3), (16, 6), (1, 1)], [(5, 8), (2, 7), (13, 6), (9, 2)]]
-#map_ = [[(-1, 0), (2, 4), (9, 3), (10, 1)], [(6, 1), (12, 2), (1, 8), (14, 7)], [(16, 1), (5, 2), (15, 6), (13, 1)], [(3, 4), (4, 3), (11, 4), (0, 7)]]
+#map_ = [[(12, 6), (14, 5), (4, 5), (6, 7)], [(15, 1), (11, 7), (3, 7), (7, 5)], [(10, 3), (8, 3), (16, 6), (1, 1)], [(5, 8), (2, 7), (13, 6), (9, 2)]]
+
 move = [(0,0),(-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(1,1),(0,1),(-1,1)]
 first_eat = map_[0][0][0]
 shark_move = map_[0][0][1]
@@ -51,21 +56,24 @@ fish_nums = [i for i in range(1,17)]
 
 def DFS(shark_r,shark_c,shark_move,fish_map,cnt):
     global answer
-    new_map = copy.deepcopy(fish_map)
     can_move = False
-    
     for i in range(1,4):
+        new_map = copy.deepcopy(fish_map)
+        new_cnt = cnt
+        
         nr = shark_r + (move[shark_move][0]*i)
         nc = shark_c + (move[shark_move][1]*i)
 
-        if nr < 0 or nc > 3 or nr < 0 or nr > 3:
+        if nr not in [0,1,2,3]:
             continue
-    
+        if nc not in [0,1,2,3]:
+            continue
         if new_map[nr][nc][0] not in fish_nums:
             continue
 
+        new_map = copy.deepcopy(fish_map)
         new_move = new_map[nr][nc][1] #먹은 물고기의 방향
-        cnt += new_map[nr][nc][0] #먹은 물고기 번호
+        new_cnt += new_map[nr][nc][0] #먹은 물고기 번호
         
         new_map[shark_r][shark_c] = (-1,0) #상어가 있던곳 빈공간 처리
         new_map[nr][nc] = (0,new_move) #상어가 도착한 곳 상어로 처리
@@ -73,13 +81,18 @@ def DFS(shark_r,shark_c,shark_move,fish_map,cnt):
         can_move = True
         
         new_map = move_fish(new_map) #먹었으면 물고기 움직이기
-        print(cnt,new_map)
-        DFS(nr,nc,shark_move,new_map,cnt) #먹은 위치에서 다시 반복
+        DFS(nr,nc,new_move,new_map,new_cnt) #먹은 위치에서 다시 반복
     
-    if not can_move:
+    if not can_move: #못 움직였으면
         answer = max(answer,cnt)
 
 move_map = move_fish(map_)
-DFS(0,0,shark_move,move_map,0)
+DFS(0,0,shark_move,move_map,first_eat)
 
 print(answer)
+    
+                
+            
+            
+            
+        
